481fd0a35776a2d7537b4d22985148ae677edbd0
WALTER SILVESTRE COAN <walter.s@univille.br>
2025-05-21 22:06:14 -0300

    microkernel

---
+++ microkernel-kernel/src/main/java/br/univille/microkernel_kernel/controller/HomeControllerAPI.java
@@ -0,0 +1,34 @@
+package br.univille.microkernel_kernel.controller;
+
+import java.util.Arrays;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import br.univille.microkernel_interface.service.DefaultService;
+
+@RestController
+@RequestMapping("/")
+public class HomeControllerAPI {
+
+    @Autowired(required = false)
+    private Map<String, DefaultService> services;
+
+    @GetMapping
+    public ResponseEntity<String> index(){
+        String serviceList = null;
+        if (services != null){
+            serviceList = Arrays.toString(services.keySet().toArray());
+            for(var oneService : services.values()){
+                oneService.doWork(null);
+            }
+            System.out.println(services.keySet());
+            
+        }
+        return ResponseEntity.ok("Microkernel API is running! " + serviceList);
+
+    }
+}
